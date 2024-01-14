84560e134874d8355e83f9271b4b7ec2ee40a7d1
Adrian <ortizdev@proton.me>
2024-01-14 20:55:00 +0100

    CSS cambios y href

--- PAGINA/app.js
+++ PAGINA/app.js
@@ -1,36 +1,33 @@
 const getData = async () => {
-  
-    const data = []; //array vacio para push
-    const arrayCoins = [
-      { name: "bitcoin", image: "./img/btc.png" },
-      { name: "ethereum", image: "./img/eth.png" },
-      { name: "binance-coin", image:"./img/bnb.png"},
-      { name: "solana", image: "./img/solana.png"},
-      { name: "xrp", image: "./img/xrp.png"},
-      { name: "usd-coin", image:"./img/usd.png"},
-      { name: "cardano", image: "./img/cardano.png"},
-      { name: "avalanche", image: "./img/avalanche.png"},
-      { name: "polkadot", image:"./img/polkadot.png"},
-      { name: "polygon", image: "./img/polygon.png"},
-      { name: "chainlink", image: "./img/chainlink.png"},
-      { name: "near-protocol", image:"./img/near.png"}
+  const data = []; //array vacio para push
+  const arrayCoins = [
+    { name: "bitcoin", image: "./img/btc.png" },
+    { name: "ethereum", image: "./img/eth.png" },
+    { name: "binance-coin", image: "./img/bnb.png" },
+    { name: "solana", image: "./img/solana.png" },
+    { name: "xrp", image: "./img/xrp.png" },
+    { name: "usd-coin", image: "./img/usd.png" },
+    { name: "cardano", image: "./img/cardano.png" },
+    { name: "avalanche", image: "./img/avalanche.png" },
+    { name: "polkadot", image: "./img/polkadot.png" },
+    { name: "polygon", image: "./img/polygon.png" },
+    { name: "chainlink", image: "./img/chainlink.png" },
+    { name: "near-protocol", image: "./img/near.png" },
+  ];
 
-    ];
+  for (let i = 0; i < arrayCoins.length; i++) {
+    const coin = arrayCoins[i].name;
+    const response = await fetch(`https://api.coincap.io/v2/assets/${coin}`);
+    const result = await response.json();
+    const formattedPrice = parseFloat(result.data.priceUsd).toFixed(2); // metodo para formatear precio
+    data.push({
+      id: result.data.id,
+      priceUsd: formattedPrice,
+      image: arrayCoins[i].image,
+    });
+  }
 
-    for (let i = 0; i < arrayCoins.length; i++) {
-      const coin = arrayCoins[i].name;
-      const response = await fetch(`https://api.coincap.io/v2/assets/${coin}`);
-      const result = await response.json();
-      const formattedPrice = parseFloat(result.data.priceUsd).toFixed(2) // metodo para formatear precio
-      data.push({
-        id: result.data.id,
-        priceUsd: formattedPrice,
-        image: arrayCoins[i].image,
-      });
-    }
-
-    mappeoDato(data);
-  
+  mappeoDato(data);
 };
 
 const mappeoDato = (data) => {
@@ -49,11 +46,13 @@ const printGallery = (dataPrint) => {
 
   dataPrint.forEach((response) => {
     const figure = `
+    <a id="api" href="https://api.coincap.io/v2/assets">
       <figure>
         <img src="${response.image}" alt="${response.id}" />
         <h3>${response.id}</h3>
         <p>Precio: $${response.priceUsd}</p>
       </figure>
+      <a>
     `;
     document.getElementById("app").innerHTML += figure;
   });
